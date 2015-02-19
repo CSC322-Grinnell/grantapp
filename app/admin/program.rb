1e806d6e270b6c16d31199872711f2fb0c51c70b
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-02-19 15:47:13 -0600

    permit program params

--- app/admin/program.rb
+++ app/admin/program.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register Program do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
+  permit_params :title, :short_description, :long_description, :picture,:deadline,:tags 
   # permit_params :list, :of, :attributes, :on, :model
   #
   # or
