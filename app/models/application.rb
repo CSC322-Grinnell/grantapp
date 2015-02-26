06f9a342978416e025f8ff8a4bfc7e5eba6410c0
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-02-25 21:09:07 -0600

    change default route to program index

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,4 @@
 class Application < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :program
+  belongs_to :user, :foreign_key => "id", :class_name => "User"
+  belongs_to :program, :foreign_key => "id", :class_name => "Program"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   resources :users
 
+  get '/', :to => 'programs#index'
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
