9e0dd7de92514f78209dbaafd3c6ea72ebe4dd85
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-04-02 16:07:03 -0500

    worked with apply button

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,10 +1,26 @@
 class AppsController < InheritedResources::Base
+
   load_and_authorize_resource
   private
 
     def app_params
-      params.require(:app).permit(:user_id, :program_id)
+      params.require(:app).permit(:user_id, :program_id, :text)
+    end
+   
+    # def create 
+   	#   @user_id = :user_id
+   	#   @program_id = :program_id
+   	#   @text = :text
+    # end
+public
+    def new
+    	debugger
+    	@app = @current_user.apps.build
     end
 
+    def create
+    	@app.program_id = params[:program].id
+    	redirect_to apps_path
+    end
 end
 
