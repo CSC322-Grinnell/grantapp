ade544376607bebb0a752cb80e7a9db07228b773
Marcel Champagne <champagn@cs.grinnell.edu>
2015-04-02 13:18:29 -0500

    added load_and_auth for programs and apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,9 +1,10 @@
 class AppsController < InheritedResources::Base
-load_and_authorize_resource
+  load_and_authorize_resource
   private
 
     def app_params
       params.require(:app).permit(:user_id, :program_id)
     end
+
 end
 
--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,5 @@
 class ProgramsController < InheritedResources::Base
-
+  load_and_authorize_resource
   private
 
     def program_params
