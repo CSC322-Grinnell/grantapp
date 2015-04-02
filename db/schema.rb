883b75629f2c0253b6ea6153af32db7fa3a805a9
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-04-02 14:35:16 -0500

    add mock field to apps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312013528) do
+ActiveRecord::Schema.define(version: 20150402193327) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150312013528) do
     t.integer  "program_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "text"
   end
 
   add_index "apps", ["program_id"], name: "index_apps_on_program_id"
