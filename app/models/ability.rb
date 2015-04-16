da51b9ccb5d0765f2d501787135c28b1ed3068bc
hansunho <hansunho@grinnell.edu>
2015-04-16 13:39:47 -0500

    changed default permissions if not loggin in

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user( not logged in)
-    if user.role == 'applicant'
+    #user ||= User.new # guest user( not logged in)
+    if user.nil?
+      can :read, Program
+    elsif user.role == 'applicant'
       can :manage, App, :user => user
       can :read, Program
     elsif user.role == 'reviewer'
       can :read, App, :program => user.program_ids[0]
       can :read, Program
-    else
-      can :read, Program
-    end
+    end 
   end
 end
