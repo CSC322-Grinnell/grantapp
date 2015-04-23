fd66e15c0f9629f3c64297621f05cd9b0d92d735
Marcel Champagne <champagn@cs.grinnell.edu>
2015-04-22 22:56:09 -0500

    add cucumber test for adding programs to reviewer

--- features/step_definitions/reviewer_program_steps.rb
+++ features/step_definitions/reviewer_program_steps.rb
@@ -2,6 +2,10 @@ Then /^(?:|I )should see a drop-down menu labeled "(.*)"$/ do |label|
   page.has_field?(label)
 end
 
+Then /^(?:|I )should see a check-box menu labeled "(.*)"$/ do |label|
+  page.has_field?(label)
+end
+
 When /^(?:|I )follow one of the "([^"]*)" links$/ do |link|
   first(:link, link).click
 end
@@ -10,7 +14,18 @@ When /^(?:|I )select "([^"]*)" from the drop-down menu labeled "(.*)"$/ do |opti
 	page.find_field(label).select(option)
 end
 
+When /^(?:|I )select "([^"]*)" from the check-box menu labeled "(.*)"$/ do |option, label|
+	check = Program.where(title: option).first.id
+	check = 'user_program_ids_' + check.to_s
+	page.check('football')
+end
+
 And /^(?:|I )should see that the "([^"]*)" is "([^"]*)"$/ do |field, value|
 	page.text.should match(/#{field}.*#{value}/mi)
 end
 
+And /^(?:|I )should see that the "([^"]*)" has "([^"]*)"$/ do |field, value|
+	check = Program.where(title: value).first.id
+	check = 'user_program_ids_' + check.to_s
+	page.find("label[for=#{check}]").should have_checked_field(value)
+end
