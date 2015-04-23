253650a231818d3b771bd1f6e9ad02525d750586
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-04-23 14:29:39 -0500

    make Rubocop happy

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,9 @@
+# User table for Active Admin
 ActiveAdmin.register User do
   actions :all, :except => [:new]
-  permit_params :first_name, :last_name, :email, :organization, :phone_number, :address, :role, program_ids: []
+  permit_params :first_name, :last_name,
+                :email, :organization, :phone_number, :address,
+                :role, program_ids: []
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,19 +16,20 @@ ActiveAdmin.register User do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-form do |f| f.inputs do
-    f.input :first_name
-    f.input :last_name
-    f.input :email
-    f.input :organization
-    f.input :phone_number
-    f.input :address
-    f.input :role,  :as => :select, :collection => ["applicant", "reviewer"]
-    f.inputs "Review Privileges" do
-    f.input :program_ids, :as => :check_boxes, :collection => Program.all, :label => "Program"
-    end 
-   end
+  form do |f| f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :organization
+      f.input :phone_number
+      f.input :address
+      f.input :role,  :as => :select, :collection => ['applicant', 'reviewer']
+      f.inputs 'Review Privileges' do
+      f.input :program_ids, :as => :check_boxes,
+              :collection => Program.all, :label => 'Program'
+      end
+     end
     f.actions
-end
+  end
 
 end
