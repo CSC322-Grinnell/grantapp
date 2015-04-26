ccc5f78c4f3bb94971d5ebfea6206c8b53103b0b
Vasilisa Bashlovkina <bashlovk@grinnell.edu>
2015-04-26 15:56:02 -0500

    branch for viewing an app as a reviewer

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -16,20 +16,20 @@ ActiveAdmin.register User do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-  form do |f| f.inputs do
+  form do |f| 
+    f.inputs do
       f.input :first_name
       f.input :last_name
       f.input :email
       f.input :organization
       f.input :phone_number
       f.input :address
-      f.input :role,  :as => :select, :collection => ['applicant', 'reviewer']
+      f.input :role,  as: :select, collection: ['applicant', 'reviewer']
       f.inputs 'Review Privileges' do
-      f.input :program_ids, :as => :check_boxes,
-              :collection => Program.all, :label => 'Program'
+        f.input :program_ids, :as => :check_boxes,
+                :collection => Program.all, :label => 'Program'
       end
-     end
+    end
     f.actions
   end
-
 end
