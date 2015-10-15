4e96605f0ad2de4e26e1cf9892ac50a727e4f1d1
Erin Gaschott <ecgaschott@gmail.com>
2015-10-15 17:21:19 +0000

    Added downloads for rubric and application form

--- app/models/program.rb
+++ app/models/program.rb
@@ -5,8 +5,10 @@ class Program < ActiveRecord::Base
   has_attached_file :application_form
   has_attached_file :rubric
   
+  Paperclip::Attachment.default_options[:default_url] = "/"
+  
   validates_attachment :application_form, presence: true, content_type: { content_type: ['text/plain', 'application/pdf']}
-  validates_attachment :rubric, presence: true, content_type: { content_type: ['text/plain', 'application/pdf']}
+  validates_attachment :rubric, content_type: { content_type: ['text/plain', 'application/pdf']}
   
   
 end
