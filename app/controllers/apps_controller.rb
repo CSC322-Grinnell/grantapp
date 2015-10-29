25dc8bbd0aacc029524ccf7eb0aa5eb8df974e23
Michael Owusu <mkowusu@gmail.com>
2015-10-29 20:15:25 +0000

    Iteration 2 - Oodles of bugfixes, fileupload/download, new tabs on some links, prevent crashes and empty downloads

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -16,15 +16,22 @@ class AppsController < InheritedResources::Base
     end
 
     def create
-      @program = Program.find_by_id(flash[:prog])
-      @app.user_id = @current_user.id
-      @app.program_id = flash[:prog]
-      @current_user.apps << @program.apps.build(app_params)
       
-      redirect_to apps_path, notice => 'File successfully uploaded.'
+      @program = Program.find_by_id(flash[:prog])
+      
+      if !@program.nil? 
+        @app.user_id = @current_user.id
+        @app.program_id = flash[:prog]
+        @current_user.apps << @program.apps.build(app_params)
+        redirect_to apps_path, :notice => 'File successfully uploaded.'
+      else
+        redirect_to apps_path, :alert => 'File upload failed - Please navigate to the upload page and try again.'
+      end
+      
     end
 
     def download
       redirect_to apps_path
     end
+    
 end
