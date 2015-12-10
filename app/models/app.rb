8b912dbb440f2e651ce76ec9495904d8a02b92b7
Marcel Champagne <voiceofmarcel@gmail.com>
2015-12-10 18:13:46 +0000

    Added :after_destroy method to app.rb, changed feedback index page

--- app/models/app.rb
+++ app/models/app.rb
@@ -5,7 +5,14 @@ class App < ActiveRecord::Base
   has_many :feedback
   
   has_attached_file :user_app
-  
   validates_attachment :user_app, presence: true, content_type: { content_type: ['text/plain', 'application/pdf']}
+  after_destroy :delete_dependent_feedback
+  
+  def delete_dependent_feedback
+      @feedbacks = Feedback.where app_id: self.id
+      @feedbacks.each do |feedback|
+        feedback.destroy
+      end
+  end
    
 end
