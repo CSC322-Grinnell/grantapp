138d4bc295d9489601472ed2e93ec1d67b2066d0
Marcel Champagne <voiceofmarcel@gmail.com>
2015-12-10 08:42:17 +0000

    Added/Fixed Existing Edit/Create/Update Methods for Applications -- Tweaked Feedback Views

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,6 +9,15 @@ class AppsController < InheritedResources::Base
     end
 
   public
+  
+    def create
+      @app = App.new(app_params)
+      if @app.save
+        redirect_to app_path(@app)
+      else
+        redirect_to new_app_path(:program_id => params[:app][:program_id]), alert: "You have uploaded an invalid file type. Only .PDF and .DOC are allowed. Try again."
+      end
+    end
 
     def new
       @program = params[:program_id]
@@ -16,8 +25,18 @@ class AppsController < InheritedResources::Base
     end
     
     def edit
-      @program = @app.program.id
-      @user = @app.user.id
+      @app = App.find(params[:id])
+      @program = @app.program_id
+      @user = @current_user
+    end
+  
+    def update
+      @app = App.find(params[:id])
+      if @app.update_attributes(app_params)
+        redirect_to app_path(@app)
+      else
+        redirect_to edit_app_path, alert: "You have uploaded an invalid file type. Only .PDF and .DOC are allowed. Try again."
+      end
     end
 
     def download
