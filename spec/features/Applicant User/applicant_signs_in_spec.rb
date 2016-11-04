36ced94bf070b04a275e0ac92aca69dd6a2bad05
Alex French <frenchal17@grinnell.edu>
2016-11-04 18:29:58 +0000

    Copy sign-in spec; make folders visible

---
+++ spec/features/Applicant User/applicant_signs_in_spec.rb
@@ -0,0 +1,40 @@
+# spec/features/user_signs_in.rb
+require 'rails_helper.rb'
+require 'spec_helper.rb'
+
+feature 'User signs in' do
+    
+    before do
+        @user = User.create(first_name: "Bob", last_name: "Example", email: "user@example.com", password: "password");
+        # FactoryGirl.create(@user);
+    end
+    
+    #given!(:user) { FactoryGirl.create(:user) }
+    
+    scenario 'with valid email and password' do
+        puts(@user.last_name);
+        visit "/users/sign_in"
+        fill_in "Email", :with => "user@example.com"
+        fill_in "Password", :with => "password"
+        click_button "Log in"
+        puts(current_path)
+        #expect(current_path).to eq('/')
+        expect(page).to have_content "Welcome"
+    end
+    
+    scenario 'with valid email but not matching passwords' do
+        visit "/users/sign_in"
+        fill_in "Email", :with => "user@example.com"
+        fill_in "Password", :with => "wrongpassword"
+        click_button "Log in"
+        expect(page).to have_text('Invalid email or password')
+    end
+    
+    scenario 'with invalid email' do
+        visit "/users/sign_in"
+        fill_in "Email", :with => 'not an email'
+        fill_in 'Password', :with => 'password'
+        click_button "Log in"
+        expect(page).to have_text('Invalid email or password')
+    end
+end
