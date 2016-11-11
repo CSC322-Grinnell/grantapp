8280cdd5e48eb34c582468a0e44f645fe78f744d
Madeleine Hardt <hardtmad@grinnell.edu>
2016-11-11 18:45:46 +0000

    Add files for reviewer tests

---
+++ spec/features/Reviewer User/reviewer_navigation_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper.rb'
+require 'spec_helper.rb'
+
+# This file tests the ability of a User with a reviewer role to sign in with 
+# valid credentials, and ensures sign-in page checks validity of email and 
+# password. This test suite uses RSpec Capybara framework
+
+feature 'Reviewer signs in' do
+    
+    # Create a user in the mock database to test signing in with valid credentials
+    before do
+        @user = User.create(first_name: "Reviewer", last_name: "Example", email: "reviewer@example.com", password: "password", role: "reviewer");
+    end
+    
+    scenario 'with valid email and password' do
+        puts(@user.last_name);
+        visit "/users/sign_in"
+        fill_in "Email", :with => "reviewer@example.com"
+        fill_in "Password", :with => "password"
+        click_button "Log in"
+        puts(current_path)
+        # The only view difference between applicant and reviewer
+        expect(page).to have_content "View Applications"
+    end
+    
+    scenario 'with valid email but not matching passwords' do
+        visit "/users/sign_in"
+        fill_in "Email", :with => "reviewer@example.com"
+        fill_in "Password", :with => "wrongpassword"
+        click_button "Log in"
+        expect(page).to have_text('Invalid email or password')
+    end
+    
+    scenario 'with invalid email' do
+        visit "/users/sign_in"
+        fill_in "Email", :with => 'not an email'
+        fill_in 'Password', :with => 'password'
+        click_button "Log in"
+        expect(page).to have_text('Invalid email or password')
+    end
+end
