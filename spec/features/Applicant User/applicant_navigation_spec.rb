7f406325b3492d2e7cd7fcbc495d2665bae20238
Alex French <frenchal17@grinnell.edu>
2016-11-11 20:36:18 +0000

    Fix spacing error; begin applicant feature testing

---
+++ spec/features/Applicant User/applicant_navigation_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper.rb'
+require 'spec_helper.rb'
+
+feature 'User navigates from home page' do
+    
+end
+
+feature 'User navigates from applying page' do
+
+end
+
+feature 'User navigates from applications page' do
+    
+end
---
+++ spec/features/Applicant User/applicant_submit_application_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper.rb'
+require 'spec_helper.rb'
+
+feature 'User downloads rubric' do
+    
+end
