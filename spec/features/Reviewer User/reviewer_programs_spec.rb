47d0ddfe2e9a2b16a15841f8366fef5a8e2ed8cb
Madeleine Hardt <hardtmad@grinnell.edu>
2016-11-16 20:08:44 +0000

    Add non-working reviewer programs test file

--- spec/features/Reviewer User/reviewer_programs_spec.rb
+++ spec/features/Reviewer User/reviewer_programs_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper.rb'
 require 'spec_helper.rb'
+require 'date'
 
 # This file tests the ability of a User with a reviewer role to view, sort, 
 # and download applications and rubrics. 
@@ -9,16 +10,40 @@ feature 'Reviewer programs' do
     
     # Create a user in the mock database to test signing in with valid credentials
     before do
-        @user = User.create(first_name: "Reviewer", last_name: "Example", email: "reviewer@example.com", password: "password", role: "reviewer");
+        #Create program
+        @pdf = File.new(Rails.root + "spec/fixtures/test_program.pdf")
+        @datetime = DateTime.new(1970, 1, 1)
+        @program = Program.create(title:"TestProgram", short_description:"Short", long_description:"Long", deadline: @datetime, application_form: @pdf)
+        
+        #Create user
+        @user = User.create(first_name: "Reviewer", last_name: "Example", email: "reviewer@example.com", password: "password", role: "reviewer")
+        
+        #Sign in user
+        visit "/users/sign_in"
+        fill_in "Email", :with => "reviewer@example.com"
+        fill_in "Password", :with => "password"
+        click_button "Log in"
     end
     
-    # Sign in user
-    before do 
+    scenario 'view programs' do
+        expect(page).to have_content "Programs"
+    end
+    
+    scenario 'sort programs' do 
+        #find(@).click
+    end
+    
+    scenario 'view long description' do
+        click_on(@program.title)
+        expect(page).to have_content "Long"
+    end
+    
+    scenario 'download application' do 
         
     end
     
-    scenario 'program' do
-
+    scenario 'download rubric' do 
+        
     end
     
 end
