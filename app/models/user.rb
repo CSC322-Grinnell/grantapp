322fa684aa5fe273f3023a35ac327f25329619d1
grosoon <bourgeoi1@grinnell.edu>
2017-11-09 15:48:47 +0000

    fixed error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ActiveRecord::Base
   validates_presence_of :last_name
   validates_presence_of :phone_number
   validates_presence_of :email
-  validates_uniqueness_of :email
   validates_presence_of :address
   
   
