ad1144a69ecee2f280fdbf99a6df521332c731cc
grosoon <bourgeoi1@grinnell.edu>
2017-11-21 16:35:39 +0000

    adding default role

--- db/migrate/20171121163237_set_default_role.rb
+++ db/migrate/20171121163237_set_default_role.rb
@@ -1,5 +1,5 @@
 class SetDefaultRole < ActiveRecord::Migration
   def change
-  	change_column :users, :role, :string, :default => 'applicant'
+    change_column :users, :role, :string, :default => 'applicant'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121152827) do
+ActiveRecord::Schema.define(version: 20171121163237) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -129,13 +129,13 @@ ActiveRecord::Schema.define(version: 20171121152827) do
     t.string   "organization"
     t.string   "phone_number"
     t.string   "address"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "encrypted_password",     default: "",          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,           null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(version: 20171121152827) do
     t.string   "state"
     t.string   "city"
     t.string   "zip_code"
-    t.string   "role"
+    t.string   "role",                   default: "applicant"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
