753c2b0204cc0f20d0806776d5bc459e462c8e33
Cassie Koomjian <cassie@substantial.com>
2019-03-07 15:06:59 -0800

    Fix creating feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,20 +1,19 @@
 class FeedbacksController < InheritedResources::Base
   load_and_authorize_resource
-  
+
   private
 
     def feedback_params
       params.require(:feedback).permit(:user_id, :app_id, :comment, :score)
     end
-    
+
   public
-  
+
     def new
-       # @app = params[:app_id]
-      @app = params.permit(:user_id, :app_id, :comment, :score) #removed .require(:feedback) as it was blocking the ability to do feebacks. 
+      @app = params[:app_id]
       @user = @current_user
     end
-    
+
     def edit
       @app = @feedback.app.id
       @user = @feedback.user
